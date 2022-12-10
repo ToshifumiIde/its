70f08eec7e530fa7d96e59dbb99a852c0a8d993b
井手俊文 <toshifumiide198765@gmail.com>
2022-12-10 10:03:19 +0900

    課題作成アプリ生成完了

--- src/main/java/com/example/its/domain/issue/IssueRepository.java
+++ src/main/java/com/example/its/domain/issue/IssueRepository.java
@@ -14,6 +14,7 @@ import java.util.List;
 public interface IssueRepository {
 
     /**
+     * issueテーブルから課題を全件検索します
      * @Selectアノテーション：メソッドが実行された際にアノテーション内のSQLが実行される
      * @return @SelectアノテーションのSQLを実行し、issuesのEntityを返却します
      */
@@ -21,6 +22,7 @@ public interface IssueRepository {
     public List<IssueEntity> findAll();
 
     /**
+     * issueテーブルに課題のレコードを1件挿入します
      * @Insertアノテーション：メソッドが実行された時にアノテーション内のSQLが実行される
      * メソッドの引数で渡ってきた値をinsert文に渡すには、MyBatisの記法で#{値}で渡すことが可能
      * @param summary 課題のサマリー
@@ -28,4 +30,12 @@ public interface IssueRepository {
      */
     @Insert("insert into issues (summary , description)values(#{summary} , #{description})")
     public void insert(String summary , String description);
+
+    /**
+     * issueテーブルから、課題IDを指定して1件レコードを取得します
+     * @param issueId 課題ID
+     * @return IssueEntity 課題IDで検索した課題のエンティティ
+     */
+    @Select("select * from issues where id = #{issueId}")
+    public IssueEntity findById(long issueId);
 }
--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,15 +1,13 @@
 package com.example.its.web.issue;
 
+import com.example.its.domain.issue.IssueEntity;
 import com.example.its.domain.issue.IssueService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -36,5 +34,12 @@ public class IssueController {
         if (bindingResult.hasErrors()) return showCreationForm(form);
         issueService.create(form.getSummary(), form.getDescription());
         return "redirect:/issues";
+    }
+
+    // GET localhost:8080/issues/{issueId}
+    @GetMapping("/{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model) {
+        model.addAttribute("issue", issueService.findById(issueId));
+        return "issues/detail";
     }
 }
